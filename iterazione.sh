42adf5c5021c856b67174350eeaf4676bc7d766f
Francesco Galanti <francescogalanti@VersuS.localdomain>
2024-10-03 17:32:07 +0200

    ciao

---
+++ iterazione.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#prova script con ciclo if
+
+for i in 1 2 3 4 5 
+do
+  echo "numero $i"
+done
+
+for i in 1 2 topolino pippo 3 pluto
+do
+echo "i ha valore $i"
+done
+
+for (( i=0; i<=20; i+=2)); do
+  echo "i ha valore $i"
+done
